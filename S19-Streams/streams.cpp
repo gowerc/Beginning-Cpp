1e695bd8d1848057d1c1679bb5fa3bf0ee1945c0
Craig Gower-Page <craiggower@gmail.com>
2023-12-09 10:14:16 +0000

    streams

---
+++ S19-Streams/streams.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main() {
+    
+    std::cout << "hi there";
+    std::cerr << "o no\n";
+    std::cout << "its ok" << std::endl;
+    
+    
+    
+    return 0;
+}
+
---
+++ S19-Streams/sttreams.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    fprintf(stdout, "This is to stdout. ");
+    fprintf(stderr, "This is to stderr. ");
+    fprintf(stdout, "This is also to stdout. ");
+}
+
